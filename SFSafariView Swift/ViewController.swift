5034049b2f285c7425508f0751f8a41163da7516
Aman Jain <amannayak15@gmail.com>
2015-12-02 16:20:10 +0530

    SFSafariViewController iOS 9
    SFSafariViewController

--- SFSafariView Swift/ViewController.swift
+++ SFSafariView Swift/ViewController.swift
@@ -7,18 +7,32 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func SFSafariVC(sender: AnyObject) {
+        let SFView = SFSafariViewController(URL: NSURL(string: "http://www.digitalhole.co/")!, entersReaderIfAvailable: false)
+        SFView.delegate = self
+        self.presentViewController(SFView, animated: true, completion: nil)
+    }
 
+
+    func safariViewControllerDidFinish(controller: SFSafariViewController) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
